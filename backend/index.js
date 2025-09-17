5846329a4ecd2376a599f5ebcff7ccdaf2fec6bb
Gautam pandit <gautampandit797@gmail.com>
2025-09-17 23:15:23 +0530

    Added some new feature of analysis

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 dotenv.config();
+import { Analytics } from "@vercel/analytics/react"
 
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/user.routes');
